ae96db46950b66c3beebab6aeb82b05ab4f4274a
Joseph Adoga <adoga.joseph0@gmail.com>
2024-03-11 18:56:48 -0500

    Project Start

---
+++ assets/js/app.js
@@ -0,0 +1,18 @@
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+/*                                                       */
+/*  Digi Arena (2024)                                    */
+/*  https://github.com/josephadoga/digi-arena            */
+/*                                                       */
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+'use strict';
+
+// This app requires a server to handle import statements and CORS issues
+import * as utils from './utils.js';
+
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+/*  Organizer                                            */
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+utils.print('Welcome');
