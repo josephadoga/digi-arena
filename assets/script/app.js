5fe334cfc957975a5a7f606e30a27ed333b134f3
Dupanshu <dupanshurangray@gmail.com>
2024-03-14 02:04:33 -0500

    made the banner without text

--- assets/script/app.js
+++ assets/script/app.js
@@ -8,12 +8,48 @@
 'use strict';
 
 // This app requires a server to handle import statements and CORS issues
-import * as utils from './utils.js';
 
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 /*  Organizer                                            */
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
-utils.print('Welcome');
+const slider = document.querySelector('.slider');
+
+const leftArrow = document.querySelector('.left');
+
+const rightArrow = document.querySelector('.right');
+
+const indicatorParents = document.querySelector('.controls ul');
+
+var sectionIndex = 0;
+
+function setIndex() {
+    document.querySelector('.controls .selected').classList.remove('selected');
+    slider.style.transform = 'translate(' + (sectionIndex) * -50 + '%)';
+};
+
+document.querySelectorAll('.controls li').forEach(function(indicator, ind) {
+    indicator.addEventListener('click', function() {
+        sectionIndex = ind;
+        setIndex();
+        indicator.classList.add('selected');   
+    });
+});
+
+rightArrow.addEventListener('click', function() {
+    sectionIndex = (sectionIndex < 1) ? sectionIndex + 1 : 1;
+    setIndex();
+    indicatorParents.children[sectionIndex].classList.add('selected');
+    
+});
+
+leftArrow.addEventListener('click', function() {
+    sectionIndex = (sectionIndex > 0) ? sectionIndex - 1 : 0;
+    setIndex();
+    indicatorParents.children[sectionIndex].classList.add('selected');
+    
+});
+
+
 
